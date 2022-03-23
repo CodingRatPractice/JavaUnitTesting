19f66bb869afe64dc5c0e82cd1f0772b6a6daf54
CodingRat <matt.marsland1@gmail.com>
2022-03-23 14:59:34 +0000

    Added project to Git

---
+++ src/Junit/Account.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Junit;
+
+/**
+ *
+ * @author User
+ * Class Provided by University of the West of England
+ * Demonstrates unit testing
+ */
+public class Account {
+    
+}
